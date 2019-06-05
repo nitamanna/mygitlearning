043c12d5edce2f724c64207ca3269167b86a955f
Nita Manna <nitamanna@gmail.com>
2019-06-05 07:47:52 +0000

    Revert "extra feature in the subtract function"
    This reverts commit bcbfd011ff11f03ad7e766286e9d7d6d533d277e.

--- git-test-substract.js
+++ git-test-substract.js
@@ -1,10 +1,5 @@
 // subtract.js
-// return subtraction of two numbers
-function subtract(a, b, bMINUSa) {
-    if (bMINUSa === true) {
-        return b - a;
-    }
-    else {
-        return a - b;
-    }
+//return substraction of 2 numbers
+function subtract(a, b) {
+    return a - b
 };
