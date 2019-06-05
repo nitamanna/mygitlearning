8dd8c085ccb49a22185e129d0033a67fa4b70fca
Nita Manna <nitamanna@gmail.com>
2019-06-05 10:42:17 +0000

    Commit1

--- git-test-substract.js
+++ git-test-substract.js
@@ -1,5 +1,6 @@
-// subtract.js
-//return substraction of 2 numbers
-function subtract(a, b) {
-    return a - b
+// subtract.js [dev/commit1]
+
+// return subtraction of two numbers
+function subtract(a, b, bMINUSa) {
+    return (bMINUSa) ? b - a : a - b;
 };
